37527d238565aba127e541b9d54b8afa1b753c23
Irrawa <irrawamew@163.com>
2024-08-30 13:09:36 +0800

    load anything at world scene.

--- RPG Tutorial/scripts/world.gd
+++ RPG Tutorial/scripts/world.gd
@@ -3,12 +3,14 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var current_tree = get_tree()
-	# Find the scenes in the current scene tree
-	var good_world_scene = current_tree.current_scene.find_child("GoodWorld", true, false)
+	
+	var goodworld_instance = preload("res://scene/good_world.tscn").instantiate()
+	goodworld_instance.global_position = Vector2(0, 0)
+	self.add_child(goodworld_instance)
 	
 	var player_instance = preload("res://scene/player.tscn").instantiate()
 	player_instance.global_position = Vector2(0, 0)
-	self.add_child(player_instance)
+	goodworld_instance.add_child(player_instance)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
